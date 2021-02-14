caee66169f87f64fe87243520a731004f2ab1125
BlankCloud2021 <78029266+BlankCloud2021@users.noreply.github.com>
2021-02-13 19:51:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Planets;
+namespace SpaceGameProject
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Script text = new Script();
+            Ship ship = new Ship();
+            Game game = new Game();
+
+            //Main Menu
+            //Welcomes player to adventure explains the rules And accpet a name for there ship.
+            text.Rules();
+            text.Welcome();
+
+            ship.ShipName = Console.ReadLine().ToUpper();
+            String Player = ship.DisplayShip();
+            text.WelcomeShip(Player);
+            ship.Continues();
+
+            //Start The Menu And begin the game Loop should start here.
+
+            text.MainMenu(ship.Fuel ,game.daysRemaining);
+            int uInput = int.Parse( Console.ReadLine());
+
+            switch (uInput)
+            {
+                case 1:
+                    //Check player Inventory
+                    ship.DisplayInventory();
+                    break;
+
+                case 2:
+                    ship.StoredItems();
+                    Console.WriteLine("Whould you like to store and item?\tYes/No");
+                    if (Console.ReadLine().ToLower()== "yes")
+                    {
+
+                    }
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 0:
+
+                    break;
+            }
+
+        }
+    }
+}
